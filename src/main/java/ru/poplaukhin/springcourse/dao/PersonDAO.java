390472e9f14fbf9bf8bd5340273b2ead1ff65db1
nevasik <nevasik@mail.ru>
2022-11-27 16:12:19 +0300

    Update 1

--- src/main/java/ru/poplaukhin/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/poplaukhin/springcourse/dao/PersonDAO.java
@@ -15,10 +15,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 21, "mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 35, "katy@mail.ru"));
     }
     public List<Person> index() { // вывод всех людей
         return people;
@@ -40,6 +40,8 @@ public class PersonDAO {
     public void update(int id, Person updatePerson) {
         Person personToBeUpdated = show(id); // поместили сюда человека, которого нужно обновить
         personToBeUpdated.setName(updatePerson.getName());
+        personToBeUpdated.setAge(updatePerson.getAge());
+        personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
--- src/main/java/ru/poplaukhin/springcourse/models/Person.java
+++ src/main/java/ru/poplaukhin/springcourse/models/Person.java
@@ -2,10 +2,14 @@ package ru.poplaukhin.springcourse.models;
 public class Person {
     private int id;
     private String name;
+    private int age;
+    private String email;
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
     public Person() {
     }
@@ -24,5 +28,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
